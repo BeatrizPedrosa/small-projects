9099c1086fd440971d86d067a30f3e3dcbeef0ec
Beatriz Rosa <bcpr@ymail.com>
2024-02-08 10:39:57 -0300

    Adição do projeto Questions e correção no menu

--- js/app.js
+++ js/app.js
@@ -159,3 +159,19 @@ else if (window.location.href.includes("Modal")) {
         modalOverlay.classList.remove('open-modal')
     })
 }
+
+//QUESTIONS
+else if (window.location.href.includes("Questions")) {
+    const questions = document.querySelectorAll('.question')
+    questions.forEach(function(question){
+        const btn = question.querySelector('.question-btn')
+        btn.addEventListener('click',function(){
+            questions.forEach(function(item){
+                if (item !== question){
+                    item.classList.remove('show-text')
+                }
+            })
+            question.classList.toggle('show-text')
+        })
+    })
+}

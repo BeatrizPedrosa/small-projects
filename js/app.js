132299560df76163bd611a05ec2f1cbd9e579cf3
Beatriz Rosa <bcpr@ymail.com>
2024-02-09 10:31:23 -0300

    Adição do projeto Video

--- js/app.js
+++ js/app.js
@@ -322,3 +322,25 @@ else if (window.location.href.includes("Menu")) {
         })
     }
 }
+
+//VIDEO
+else if (window.location.href.includes("Video")) {
+    const btn = document.querySelector(".switch-btn")
+    const video = document.querySelector(".video-container")
+
+    btn.addEventListener('click',function(){
+        if(!btn.classList.contains('slide')){
+            btn.classList.add('slide')
+            video.pause()
+        }
+        else {
+            btn.classList.remove('slide')
+            video.play()
+        }
+    })
+
+    const preloader = document.querySelector(".preloader")
+    window.addEventListener('load',function(){
+        preloader.classList.add('hide-preloader')
+    })
+}
